7f4422d3601ec716450017bbc226f19e96ce0249
gomez3377 <jongomez3377@gmail.com>
2021-02-06 15:53:07 -0700

    c

---
+++ Lesson 5/js/fivedays.js
@@ -0,0 +1,20 @@
+const d = new Date()
+const i = d.getDay();
+let n = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let day = [];
+
+while (day.length < 5) {
+    for (let x = 0; x < 7 
+
+    }
+}
+
+console.log(day);
