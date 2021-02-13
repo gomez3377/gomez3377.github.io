493a77141317da7ae6c324d3ec1bc4904c4af8c0
gomez3377 <jongomez3377@gmail.com>
2021-02-13 15:42:32 -0700

    c

--- Lesson 6/js/windchill.js
+++ Lesson 6/js/windchill.js
@@ -1,11 +1,15 @@
-var temp = 112;
-var windspeed = 123;
+let temp = document.getElementById("currenttemp");
+let windspeed = document.getElementById("currentwind");
 
 if (temp <= 50 && windspeed > 4.8) {
-    let chill =windchill (temp, windspeed);
-    document.getElementById('windchill').innerHTML = "Wind Chill: " + chill + "&#176;F"; 
+  let chill = windchill(temp, windspeed);
+  document.getElementById("windchill").innerHTML =
+    "Wind Chill: " + chill + "&#176;F";
+} else {
+  document.getElementById("windchill").innerHTML = "Wind Chill: N/A";
 }
 
-function windchill (){
-    let f = 35.74 + 0.6215 - Math.pow(35.75, 0.16) + Math.pow(0.42375, 0.16);
+function windchill() {
+  let c = temp + 0.6215 - Math.pow(windspeed, 0.16) + Math.pow(0.42375, 0.16);
+  return Math.round(c);
 }
