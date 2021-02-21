d445daf0c69d28553692ac86545b506983e969a4
gomez3377 <jongomez3377@gmail.com>
2021-02-20 22:52:07 -0700

    c

--- Lesson7/js/practice.js
+++ Lesson7/js/practice.js
@@ -1,11 +1,27 @@
-const pic = document.querySelectorAll("[data-src]");
-
-const callbackFunction = function (entries) {
-  console.log(entries[0]);
+const images = document.querySelectorAll('[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };  
 };
 
-const observer = new IntersectionObserver(callbackFunction, {
-  threshold: 0.4,
-});
+const imgOptions = {
+  threshold: 0.3,
+  rootMargin: "-50px"
+};
 
-observer.observe(pic);
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+entries.forEach((entry) => {
+  if (!entry.isIntersecting) {
+    return;
+  } else {
+    loadImages(entry.target);
+    imgObserver.unobserve(entry.target);
+  }
+});
+}, imgOptions);
+
+images.forEach((image) => {
+imgObserver.observe(image);
+});
